c59393e97045b95de5b6fa973c2de4d1812390a0
András Veres-Szentkirályi <vsza@vsza.hu>
2011-12-04 20:45:37 +0100

    filled measurement section

--- stat.sh
+++ stat.sh
@@ -1,2 +1,3 @@
 perl SuperBarTables.pl stat_init.csv 4.5 0 1500 1 500,1000,1500 >stat_init.tex
 perl SuperBarTables.pl stat_invoke.csv 3.5 0 1100 1 275,550,825,1100 >stat_invoke.tex
+perl SuperBarTables.pl stat_packet.csv 5.5 0 20 1 5,10,15,20 >stat_packet.tex
